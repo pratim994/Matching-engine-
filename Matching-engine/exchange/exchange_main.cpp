c8ab7fafe5449586fab7b3ac5e2df04807e8d845
pratim <aryanpratim86@gmail.com>
2026-03-21 10:33:51 +0530

    added market data  publisher and order server methods

--- Matching-engine/exchange/exchange_main.cpp
+++ Matching-engine/exchange/exchange_main.cpp
@@ -4,8 +4,12 @@
 
 
 Common::Logger* logger = nullptr;
+
 Exchange::MatchingEngine* matching_engine = nullptr;
 
+Exchange::MarketDataPublisher *market_data_publisher = nullptr;
+
+Exchange::OrderServer *order_server = nullptr;
 
 void signal_handler(int)
 {
@@ -13,9 +17,21 @@ void signal_handler(int)
     
     std::this_thread::sleep_for(10s);
 
-    delete logger; logger = nullptr;
+    delete logger;
+    
+    logger = nullptr;
 
-    delete  matching_engine; matching_engine = nullptr;
+    delete  matching_engine;
+    
+    matching_engine = nullptr;
+
+    delete market_data_publisher;
+    
+    market_data_publisher = nullptr;
+
+    delete order_server;
+
+    order_server = nullptr;
 
     std::this_thread::sleep_for(10s);
     
@@ -43,7 +59,6 @@ int main(int , char **)
         market_updates(ME_MAX_MARKET_UPDATES);
 
 
-
         std::string time_str;
 
         logger->log("%:% %() % Starting Matching Engine..\n",
@@ -56,10 +71,41 @@ int main(int , char **)
             
                 matching_engine->start();
 
+
+
+        const std::string mkt_pub_iface = "lo";
+
+        const std::string snap_pub_ip = "233.252.14.1", inc_pub_ip =  "233.252.14.3";
+
+        const int snap_pub_port = 20000 , inc_pub_port = 20001;
+
+
+        logger->log("%:% %() % Starting Market Data Publisher ...\n" , __FILE__, __LINE__, __FUNCTION__, 
+            
+                Common::getCurrentTimeStr(&time_str_));
+
+        market_data_publisher = new Exchange::MarketDataPublisher(&market_updates, mkt_pub_iface, snap_pub_ip,snap_pub_port,
+                
+                inc_pub_ip, inc_pub_port);
+
+        market_data_publisher->start();
+
+        const std::string order_gw_iface = "lo";
+
+        const int  order_gw_port  = 12345;
+
+        logger->log("%:% () % Starting order server ...\n", __FILE__, __LINE__, __FUNCTION__, Common::getCurrentTimeStr(&time_str_));
+
+        order_server = new Exchange::OrderServer(&clieent_requests, &client_responses, order_gw_iface, order_gw_port);
+
+        order_server->start();
+
+
+
                 while(true){
                     logger->log("%:% %() % Sleeping for a few milliseconds..\n,
                         ", __FILE__, __LINE__ , __FUNCTION__,
-                        COmmon::getCurrentTimeStr(&time_str));
+                        Common::getCurrentTimeStr(&time_str));
                         usleep(sleep_time*1000);
 
                     }
